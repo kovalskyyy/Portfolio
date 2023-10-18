0bced532a113501b6782192af1ea024752106dfb
Marcel <113881740+kovalskyyy@users.noreply.github.com>
2023-10-18 10:05:56 +0200

    Add files via upload

---
+++ scrpits.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+var map = L.map('map').setView([52.229676, 21.012229], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
